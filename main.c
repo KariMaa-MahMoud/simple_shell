25b2c4d0b9ca3b77864ea2fc68f9246d5210337f
KariMaa-MahMoud <128936629+KariMaa-MahMoud@users.noreply.github.com>
2023-11-15 12:17:26 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-#include "shell.h"\
+#include "shell.h"
 
 int main() {
     char command[MAX_COMMAND_LENGTH];
@@ -7,10 +7,10 @@ int main() {
         printf("$ ");
         fgets(command, MAX_COMMAND_LENGTH, stdin);
 
-        // to remove \n
+    
         command[strcspn(command, "\n")] = '\0';
 
-        // Exit the shell when entering "exit"
+
         if (strcmp(command, "exit") == 0) {
             break;
         }
