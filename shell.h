13954366b00b88213fe9d9bff5e1dbf446dcae6b
KariMaa-MahMoud <128936629+KariMaa-MahMoud@users.noreply.github.com>
2023-11-15 12:14:04 +0200

    Create shell.h

---
+++ shell.h
@@ -0,0 +1,16 @@
+#ifndef KM_SHELL_H_
+#define KM_SHELL_H_
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h> 
+#include <errno.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <process.h>
+
+#endif
